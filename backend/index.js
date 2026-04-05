9ea0895ab3a8a6aff065b13c3175a17cf6d0121f
shifantasticc <shifastudyit@gmail.com>
2026-04-05 22:22:22 +0530

    Updating backend Files

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import express from 'express';
 import { connectDB } from './config/db.js';
 
-// ⭐ ADD CLERK MIDDLEWARE
-import { clerkMiddleware } from "@clerk/express";
+// ADD CLERK MIDDLEWARE
+import { clerkMiddleware } from '@clerk/express';
 import appointmentRouter from './routes/appointmentRouter.js';
 import doctorRouter from './routes/doctorRouter.js';
 import serviceRouter from './routes/serviceRoutes.js';
@@ -13,10 +13,10 @@ import serviceAppointmentRouter from './routes/serviceAppointmentRouter.js';
 const app = express();
 const port = process.env.PORT || 4000;
 
-// ⭐ IMPORTANT: ENABLE CREDENTIALS FOR CLERK COOKIE SESSION
+// IMPORTANT: ENABLE CREDENTIALS FOR CLERK COOKIE SESSION
 const allowedOrigins = [
-  "http://localhost:5173", // user frontend
-  "http://localhost:5174", // admin dashboard
+  'https://heal-via.vercel.app/', // user frontend
+  'https://heal-via-admin.vercel.app/', // admin dashboard
 ];
 
 app.use(
@@ -29,37 +29,35 @@ app.use(
         return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS"));
+      return callback(new Error('Not allowed by CORS'));
     },
     credentials: true, // ✅ REQUIRED for cookies / Clerk
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
 );
 
-
-// ⭐ Use Clerk middleware globally (does NOT protect routes)
+// Use Clerk middleware globally (does NOT protect routes)
 app.use(clerkMiddleware());
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Database Connection
 connectDB();
 
 // Static uploads folder
 
-
 // Routes (unchanged)
-app.use("/api/appointments", appointmentRouter);
-app.use("/api/doctors", doctorRouter);
-app.use("/api/services", serviceRouter);
-app.use("/api/service-appointments", serviceAppointmentRouter);
+app.use('/api/appointments', appointmentRouter);
+app.use('/api/doctors', doctorRouter);
+app.use('/api/services', serviceRouter);
+app.use('/api/service-appointments', serviceAppointmentRouter);
 
 // Test route
 app.get('/', (req, res) => {
-    res.send('API Working ');
+  res.send('API Working ');
 });
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`);
+  console.log(`Server Started on http://localhost:${port}`);
 });
